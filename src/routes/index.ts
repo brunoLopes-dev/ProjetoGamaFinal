f688d956f4f4d3c6600949d490ed0941c82afb15
pedro <pedrokoast@gmail.com>
2023-04-09 20:46:12 -0300

    adicionei comentario da rota

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import { Router } from "express";
 import controller from '../controllers/controllerpedidos'
 const routes = Router()
 
+//rotas pedidos
 routes.post('/pedidos'), controller.create
 routes.get('/pedidos'),controller.list
 routes.get('/pedidos:id'),controller.getpedidoid
